f949bf249558e2068e973c67bec2a75e4f04394a
dqxfc <2651727804@qq.com>
2020-12-03 22:49:10 +0800

    commit second

---
+++ course.java
@@ -0,0 +1,6 @@
+import java.util.*
+class course{
+string cname;//课程名称
+string cno;//课程编号
+string cnum;//课程人数
+}
--- student.java
+++ student.java
@@ -1,5 +1,7 @@
 import java.util.*
 class student{
-private string sname;
-private string sno;
+private string sname;//学生姓名
+private string sno;//学号
+private string sphone;//学生手机号
+private string email;//学生邮箱
 }
